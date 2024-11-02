a6ffb1c107cc93e92e1cb6cde4197605068d2999
AugustStoner <jaas23@hampshire.edu>
2024-11-01 23:53:28 -0400

    Final changes and reflections before submission on 11/1

--- Library.java
+++ Library.java
@@ -1,12 +1,99 @@
 /* This is a stub for the Library class */
-public class Library {
+import java.util.Hashtable;
 
-    public Library() {
+public class Library extends Building{
+
+    private final Hashtable<String, Boolean> collection;
+
+    public Library(String name, String address, int nFloors) {
+      super(name, address, nFloors);
+      this.collection = new Hashtable<String, Boolean>();
       System.out.println("You have built a library: 📖");
     }
-  
+    
+    // Methods (modify collection)
+
+    public void addTitle(String title){
+      // check that book is not already part of the collection
+      //if (this.containsTitle(title)) { throw RuntimeException(title + " is already part of " + this.getName() + "'s collection.");}
+      this.collection.put(title, true);
+    }
+
+    public String removeTitle(String title){
+      // check that book is part of the collection
+      //if (!this.containsTitle(title)) { throw RuntimeException(title + " is not part of " + this.getName() + "'s collection.");}
+      // check that the book is available
+      //if (!this.isAvailable(title)) { throw RuntimeException(title + " is checked out, it cannot be removed at this time.");}
+      this.collection.remove(title, true); 
+      return title;
+    }
+
+    // Methods (modify value/status)
+
+    public void checkOut(String title){
+      // check that book is part of the collection
+      //if (!this.containsTitle(title)) { throw RuntimeException(title + " is not part of " + this.getName() + "'s collection.");}
+      // check that book is available to check out
+      
+      this.collection.put(title, false);
+    }
+
+    public void returnBook(String title){
+      // check that book is part of the collection
+      //if (!this.containsTitle(title)) { throw RuntimeException(title + " is not part of " + this.getName() + "'s collection.");}
+      // check that book has been checked out
+
+      this.collection.put(title, true);
+    }
+
+    // Methods (browsing)
+
+    public boolean containsTitle(String title){
+      // return true if key exists in collection
+      return this.collection.containsKey(title);
+    }
+
+    public boolean isAvailable(String title){
+      // return true if value is true, false otherwise
+      return this.collection.get(title);
+    }
+
+    public void printCollection(){
+      // prints out the entire collection in an easy-to-read way (including checkout status)
+      System.out.println("Books in the " + this.getName() + " Collection:");
+      this.collection.forEach((title, value) -> {
+        if (this.isAvailable(title)) {
+          System.out.println(title + " is available.");
+        }
+        else {
+          System.out.println(title + " is checked out.");
+        }
+      });
+    }
+
+    public String toString() {
+      return super.toString();
+    }
+
     public static void main(String[] args) {
-      new Library();
+      Library neilson = new Library("Neilson Library", "7 Neilson Drive, Northampton, MA 01063", 4);
+      neilson.addTitle("Goblin Mode by McKayla Coyle");
+      neilson.printCollection();
+      neilson.addTitle("Uglies by Scott Westerfeld");
+
+      neilson.addTitle("The Moth Keeper by K. O'Neill");
+      //System.out.println(neilson.removeTitle("Goblin Mode by McKayla Coyle"));
+      neilson.printCollection();
+      System.out.println(neilson.toString());
+      System.out.println(neilson.isAvailable("Goblin Mode by McKayla Coyle"));
+      neilson.checkOut("Goblin Mode by McKayla Coyle");
+      System.out.println(neilson.isAvailable("Goblin Mode by McKayla Coyle"));
+      neilson.returnBook("Goblin Mode by McKayla Coyle");
+      System.out.println(neilson.isAvailable("Goblin Mode by McKayla Coyle"));
+      System.out.println(neilson.containsTitle("Goblin Mode by McKayla Coyle"));
+      System.out.println(neilson.removeTitle("Goblin Mode by McKayla Coyle"));
+      System.out.println(neilson.containsTitle("Goblin Mode by McKayla Coyle"));
+
     }
   
   }
--- reflection.md
+++ reflection.md
@@ -2,3 +2,10 @@ Use this file to record your reflection on this assignment.
 
 - What are your initial impressions of how `Inheritance` affects your programming?
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+
+Inheritance is a cool concept, and I like how it impacts the workflow of creating similar objects. 
+I did not get to a level of error handling in this assignment as I would have liked, which will likely cause issues until I sit down and implement better error handling. 
+Partly I think the automatic Hashtable exceptions will cover certain cases, but I have not determined which situations should be handled separately. 
+My code is very messy and not well-documented, and I am not satisfied with my ability to prevent future issues by handling extra information in advance, as I typically try to. 
+My process for creating algorithms/outlines for the classes in this assignment worked well to keep track of which required attributes and methods I had completed, but I was not able to keep track of as much helpful information to make sure everything worked as well as it could have. 
+For someone taking this course in the future, plan to give yourself time to create algorithms and work incrementally, reviewing your work and making sure it is readable and sensible as you go (as I did not do). 
